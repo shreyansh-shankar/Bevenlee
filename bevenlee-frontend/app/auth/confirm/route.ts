7222a8141516bb847dee887d8e23990526ac8651
shreyansh-shankar <shreyanshshankar257@gmail.com>
2026-02-04 21:13:04 +0530

    updated route.ts

--- bevenlee-frontend/app/auth/confirm/route.ts
+++ bevenlee-frontend/app/auth/confirm/route.ts
@@ -5,26 +5,20 @@ import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
+
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = searchParams.get("next") ?? "/";
 
   if (token_hash && type) {
     const supabase = await createClient();
 
-    const { error } = await supabase.auth.verifyOtp({
-      type,
+    // This VERIFY step confirms the email (DB level)
+    await supabase.auth.verifyOtp({
       token_hash,
+      type,
     });
-    if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next);
-    } else {
-      // redirect the user to an error page with some instructions
-      redirect(`/auth/error?error=${error?.message}`);
-    }
   }
 
-  // redirect the user to an error page with some instructions
-  redirect(`/auth/error?error=No token hash or type`);
+  // ALWAYS redirect to login
+  redirect("/auth/login?verified=1");
 }
